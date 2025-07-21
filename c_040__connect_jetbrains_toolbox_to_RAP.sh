46d18e843db1a0c131c6be747044b713fdecaa38
Mahdi <imahdimir@gmail.com>
2025-07-20 19:23:48 -0700

    update

--- c_040__connect_jetbrains_toolbox_to_RAP.sh
+++ c_040__connect_jetbrains_toolbox_to_RAP.sh
@@ -1,3 +1,8 @@
-dx describe job
 
-ssh -i ~/.dnanexus_config/ssh_id dnanexus@
+# run this in the dx venv and get the host address: [ec2-3-8-203-151.eu-west-2.compute.amazonaws.com]
+dx describe job-J1vpJkjJQp6V9VFkPFPq74qG
+
+# run this in the toolbox
+ssh -i ~/.dnanexus_config/ssh_id dnanexus@ec2-3-8-203-151.eu-west-2.compute.amazonaws.com
+
+ssh -N -L 8888:localhost:8888 -i ~/.dnanexus_config/ssh_id dnanexus@ec2-3-8-203-151.eu-west-2.compute.amazonaws.com
